398b3603d05c79439abbeadca4ffdde551b2f55f
raffuru <56018.ishida@gmail.com>
2022-03-17 16:49:43 +0900

    sec

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// トップページスライドショー
 $('.slider').slick({
     fade:true,
     autoplay:true,
@@ -10,3 +11,68 @@ $('.slider').slick({
     infinite:true,
     pauseOnHover:false,
 })
+
+
+// ナビゲーションのフェード設定
+$(function(){
+    var pos = 0;
+    var header = $('.header');
+    
+    $(window).on('scroll', function(){
+      if($(this).scrollTop() < pos ){
+        //上にスクロールしたとき
+        header.removeClass('hide');
+      }else{
+        //下にスクロールしたとき
+        header.addClass('hide');
+      }
+      pos = $(this).scrollTop();
+    });
+  });
+
+
+// 要素間の境界wavify(うまく行ってない)
+  $('#wave').wavify({
+    height: 60,
+    bones: 3,
+    amplitude: 40,
+    color: '#fff',
+    speed: .25,
+  });
+
+//   全要素のフェードイン設定
+// $(function (){
+//     $(window).scroll(function (){
+//         const wHight = $(window).height();
+//         const scrollAmount = $(window).scrollTop();
+
+//         let buffer;
+//         if ($(window).width() > 768){
+//             buffer = 300;
+//         } else{
+//             buffer = 100;
+//         }
+
+//         $('.element').each(function(){
+//             const targetPosition = $(this).offset().top;
+//             if (scrollAmount > targetPosition - wHight + buffer){
+//                 $(this).addClass("is-fadein");
+//                 console.log(buffer);
+//             }
+//         });
+//     });
+// });
+
+$(function () {
+    $(window).scroll(function () {
+      const windowHeight = $(window).height();
+      const scroll = $(window).scrollTop();
+  
+      $('.element').each(function () {
+        const targetPosition = $(this).offset().top;
+        if (scroll > targetPosition - windowHeight + 100) {
+          $(this).addClass("is-fadein");
+        }
+      });
+    });
+  });
